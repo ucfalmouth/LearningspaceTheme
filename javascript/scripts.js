7a0cbc1293d2f16ecb9c2a0136d4216ec5b13e7c
Aaron Marr <aaronmarruk@gmail.com>
2014-03-11 14:20:48 +0000

    Ajax-up the form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -451,4 +451,29 @@ if ($('.path-enrol').length != 0){
 
 });
 
+jQuery(document).ready(function($) {
+  $('form.quform').Quform();
+
+  // Tooltip settings
+  if ($.isFunction($.fn.qtip)) {
+    $('.quform-tooltip').qtip({
+      content: {
+        text: false
+      },
+      style: {
+        classes: 'qtip-default qtip-shadow quform-tt',
+        width: '180px'
+      },
+      position: {
+        my: 'left center',
+        at: 'right center'
+      }
+    });
+  }
+
+  // Changes subject to a text field when 'Other' is chosen
+  $('#subject').replaceSelectWithTextInput({onValue: 'Other'});
+}); // End document ready
+
+
 
