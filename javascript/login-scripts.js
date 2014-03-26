eece0d8a6309418e6d107d8e387b1c27d4704b48
Aaron Marr <aaronmarruk@gmail.com>
2014-03-26 14:24:32 +0000

    Updated countdown clock

--- javascript/login-scripts.js
+++ javascript/login-scripts.js
@@ -37,13 +37,13 @@ $(document).ready(function() {
               
               // add 3 0's
 
-              var next = 1395738000000;
+              var next = 1396429200000;
               
 
               // Midnight 16th Oct, 2013
               var now = new Date().getTime();
 
-              var prev = 1395129978000;
+              var prev = 1395843749000;
 
                 var msPerMinute = 60 * 1000;
                 var msPerHour = msPerMinute * 60;
