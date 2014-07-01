eb18c66bd8b66dad3bd2a01b1fa90ae902edb61b
Aaron Marr <aaronmarruk@gmail.com>
2014-07-01 09:31:51 +0100

    Comment out problems with this page code in pwtp.php

--- layout/problems-with-page-form.php
+++ layout/problems-with-page-form.php
@@ -1,6 +1,6 @@
-<a id="display-instant-feedback-form" href="#">Problems with this page?</a>
+<!--<a id="display-instant-feedback-form" href="#">Problems with this page?</a>
 
-<!-- To copy the form HTML, start here -->
+
 
             <div id="instant-feedback-form" class="quform-outer quform-theme-light-light section-contact__form">
             <h1>Help us improve Learning Space</h1>
@@ -9,7 +9,7 @@
                 <form class="quform" action="<?php echo $CFG->wwwroot;?>/theme/learningspace/layout/quform/process.php" method="post" enctype="multipart/form-data" onclick="">
                     <div class="quform-inner">
                             <div class="quform-elements">
-                                <!-- Begin 2 column Group -->
+                                
                                 
                                 <?php $current_page = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>
                                 
@@ -54,7 +54,7 @@
                             </div>
                         </div>
 
-                        <!-- Begin Textarea element -->
+                       
                         <div class="quform-element quform-element-textarea quform-huge">
                             <div class="quform-spacer">
                                 <div class="quform-input">
@@ -62,9 +62,9 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- End Textarea element -->
+                        
 
-                        <!-- Begin Textarea element -->
+                      
                         <div class="quform-element quform-element-textarea quform-huge">
                             <div class="quform-spacer">
                                 <div class="quform-input">
@@ -72,10 +72,10 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- End Textarea element -->
+                       
 
 
-                        <!-- Begin Textarea element -->
+                        
                         <div class="quform-element quform-element-textarea quform-huge">
                             <div class="quform-spacer">
                                 <div class="quform-input">
@@ -83,34 +83,21 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- End Textarea element -->
+                       
 
-                        <!-- Begin Submit button -->
+                       
                         <div class="quform-submit">
                             <div class="quform-submit-inner">
                                 <button type="submit" class="btn btn-primary" style="padding: 0.5rem 2rem;" value="Send"><span>Send</span></button>
                             </div>
                             <div class="quform-loading-wrap"><span class="quform-loading"></span></div>
                         </div>
-                        <!-- End Submit button -->
+                        
                         </div>
                     </div>
                 </form>
             </div>
             <br />
-           
-
-            <?php //print_r($PAGE->); 
-                //$last_mod = ($COURSE->timemodified); 
-                //echo date("dS M Y", $last_mod);
-            ?>
+        
             
-            </p>
-
-
-
-
-
-
-
-
+            </p>-->
